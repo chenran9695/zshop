2b7d49b2ee6b194d797b93c7ef16a00850100234
chenran <chenran_uestc@163.com>
2019-08-17 18:32:44 +0800

    2019年8月17日18点32分 添加了前端商城注册功能

---
+++ zshop_common/src/main/java/com/cr/zshop/common/exception/CustomerExistException.java
@@ -0,0 +1,11 @@
+package com.cr.zshop.common.exception;
+
+/**
+ * @author ：cr
+ * @date ：Created in 2019/8/17 17:53
+ * @description：异常：用户已存在$
+ * @modified By：cr
+ * @version: $
+ */
+public class CustomerExistException {
+}
