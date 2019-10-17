a8500e88c292c76b029206703949cd1eeaf1d2a3
chenran <chenran_uestc@163.com>
2019-10-17 17:59:00 +0800

    20191017-1

---
+++ zshop_common/src/main/java/com/cr/zshop/common/util/SpringBeanHolder.java
@@ -0,0 +1,11 @@
+package com.cr.zshop.common.util;
+
+/**
+ * @author ：cr
+ * @date ：Created in 2019/10/17 17:48
+ * @description：$
+ * @modified By：cr
+ * @version: $
+ */
+public class SpringBeanHolder {
+}
